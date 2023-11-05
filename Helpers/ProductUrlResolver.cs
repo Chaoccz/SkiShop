b881b7a77f6dd4ba292cfdbfe72d3465a56ca002
Chao Gao <chaogao686@gmail.com>
2023-11-05 02:52:05 -0600

    Generic Repository with Specfication Pattern set up

---
+++ Helpers/ProductUrlResolver.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Core.Entities;
+using Ecommerce.API.Dtos;
+
+namespace Ecommerce.API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            return _config["ApiUrl"] + source.PictureUrl;
+        }
+    }
+}
