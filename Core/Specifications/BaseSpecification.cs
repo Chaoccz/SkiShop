557a398de3cb26d2554ea4637d6c96b34ad77e70
Chao Gao <chaogao686@gmail.com>
2023-11-07 08:46:43 -0600

    API Error Handling

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -18,7 +18,7 @@ namespace Core.Specifications
 
         public Expression<Func<T, bool>> Criteria { get; }
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-
+        
         protected void AddInclude(Expression<Func<T, object>> include) { Includes.Add(include); }
     }
 }
