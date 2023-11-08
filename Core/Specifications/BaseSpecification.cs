ef48d058c24ed01e673bff9136940e1e86f41a1f
Chao Gao <chaogao686@gmail.com>
2023-11-08 11:04:57 -0600

    API Paging, Filtering, Sorting & Searching

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -18,7 +18,29 @@ namespace Core.Specifications
 
         public Expression<Func<T, bool>> Criteria { get; }
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
-        
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDesc { get; private set; }
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+        public bool IsPagingEnabled { get; private set; }
+
         protected void AddInclude(Expression<Func<T, object>> include) { Includes.Add(include); }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderBy)
+        {
+            OrderBy = orderBy;
+        }
+
+        protected void AddOrderByDesc(Expression<Func<T, object>> orderByDesc)
+        {
+            OrderByDesc = orderByDesc;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Take = take;
+            Skip = skip;
+            IsPagingEnabled = true;
+        }
     }
 }
--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -10,10 +10,24 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification: BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams productParams)
+        : base(x => (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search))
+            && (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId) 
+                    && (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId))
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1), productParams.PageSize);
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc": AddOrderBy(x => x.Price); break;
+                    case "priceDesc": AddOrderByDesc(x => x.Price);
+                        break;
+                    default: AddOrderBy(x => x.Name); break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
