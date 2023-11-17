d2cf0dbebe68abbd8b6f771faa89dfc405b02d11
Chao Gao <chaogao686@gmail.com>
2023-11-17 00:22:32 -0600

    Basket API

---
+++ Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            var basket = await _database.StringGetAsync(basketId);
+            return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var updated =
+                await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(7));
+            if (!updated) return null;
+            return await GetBasketAsync(basket.Id);
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+    }
+}
